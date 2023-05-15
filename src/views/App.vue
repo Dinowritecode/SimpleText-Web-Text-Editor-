d82cfff2c6a1a1c98580cacab71c5516e933f432
Dinosaur.YH <zoo.to@qq.com>
2023-05-15 21:59:10 +0800

    V0.0.0.1EA界面升级

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,17 +1,31 @@
 <template>
 <div id="interface">
 <div id="tools">
+    <img src="../assets/引用_quote.svg">
     <img src="../assets/文字加粗_text-bold.svg">
     <img src="../assets/文字斜体_text-italic.svg">
+    <img src="../assets/删除线_strikethrough.svg">
+    <img src="../assets/文字下划线_text-underline.svg">
     <img src="../assets/文字大小_add-text.svg">
+    <img src="../assets/文字_add-text-two.svg">
+    <img src="../assets/文字_text.svg">
+    <img src="../assets/背景颜色_background-color.svg">
     <img src="../assets/文字居中_align-text-center.svg">
     <img src="../assets/文字居右_align-text-right.svg">
     <img src="../assets/文字居左_align-text-left.svg">
     <img src="../assets/文字两边对齐_align-text-both.svg">
     <img src="../assets/标题级别_title-level.svg">
     <img src="../assets/列表_list-bottom.svg">
+    <img src="../assets/链接_link-one.svg">
     <img src="../assets/代码_code.svg">
+    <img src="../assets/表格_table-file.svg">
+    <img src="../assets/笑脸_smiling-face.svg">
     <img src="../assets/增加图片_add-pic.svg">
+    <img src="../assets/播放器_play-two.svg">
+    <img src="../assets/清除格式_clear-format.svg">
+    <img src="../assets/撤销_undo.svg">
+    <img src="../assets/重新_redo.svg">
+    <img src="../assets/全局放大_full-screen.svg">
 </div>
 <div id="bottom-line"></div>
 </div>
@@ -38,7 +52,6 @@ export default{
 }
 #interface > #tools{
     position: absolute;
-    display: flex;
     left: 12px;
 }
 #interface > #tools img{
